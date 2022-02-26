851544a900daea706f237d61b49411456a680870
martyners <67791771+martyners@users.noreply.github.com>
2022-02-26 13:18:37 +0100

    added font-size larde in buttons

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
 
             <Section
                 header="Lista zadań"
-                body={<List tasks={tasks} hideDoneTasks={hideDoneTasks} />}
                 extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+                body={<List tasks={tasks} hideDoneTasks={hideDoneTasks} />}
             />
 
         </Container>
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,21 +4,20 @@ const Buttons = (props) => {
     if (props.tasks.lenght === 0) {
         return null
     }
-return (
+    return (
         <div className="buttons">
             <button className="buttons__button">
                 {props.hideDoneTasks ? "Pokaż " : "Ukryj "}
-                 ukończone
+                ukończone
             </button>
             <button
-            className="buttons__button"
-            disabled={props.tasks.every(({ done }) => done )}
-               
+                className="buttons__button"
+                disabled={props.tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
             </button>
         </div>
-)
+    )
 };
 
 export default Buttons;
