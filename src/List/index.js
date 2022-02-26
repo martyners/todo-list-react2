af89cbc0d7cd272c835d0e8f097b21e48de971c1
martyners <67791771+martyners@users.noreply.github.com>
2022-02-26 13:16:52 +0100

    added formatting

--- src/List/index.js
+++ src/List/index.js
@@ -3,7 +3,8 @@ import "./style.css";
 const List = (props) => (
     <ul className="list">
         {props.tasks.map(task => (
-            <li key={task.id}
+            <li
+                key={task.id}
                 className={`list__item ${task.done && props.hideDoneTasks ? " list__item--hidden" : ""} ">`}
             >
                 <button className="list__button list__button--done">
